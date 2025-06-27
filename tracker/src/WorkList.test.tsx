37fa03a585b8d9832d83d2361a04edf8740178f8
Steven Solomon <steven.solomon@radai.com>
2025-06-27 14:44:19 +0000

    Test for work item

--- tracker/src/WorkList.test.tsx
+++ tracker/src/WorkList.test.tsx
@@ -1,12 +1,18 @@
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import WorkList from "./WorkList";
 
 describe(WorkList, () => {
-  it("Renders?", () => {
-    render(<WorkList />);
+  describe("when add clicked", () => {
+    it("creates new work item ", async () => {
+      render(<WorkList />);
 
-    expect(screen.getByTestId("work-list")).not.toBeUndefined();
-    expect(screen.getByTestId("btn-add")).not.toBeUndefined();
+      expect(screen.getByTestId("work-list")).not.toBeUndefined();
+      screen.getByTestId("btn-add").click();
+
+      await waitFor(() => {
+        expect(screen.getByTestId("work-item-1")).not.toBeUndefined();
+      });
+    });
   });
 });
