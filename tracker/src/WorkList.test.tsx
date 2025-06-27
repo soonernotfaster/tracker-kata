a10d4ddef1853c6f800aabb2a8ef5e77f56cc0ee
Steven Solomon <steven.solomon@radai.com>
2025-06-27 14:25:09 +0000

    Failing: renders component

---
+++ tracker/src/WorkList.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import WorkList from "./WorkList";
+
+describe(WorkList, () => {
+  it("Renders?", () => {
+    render(<WorkList />);
+
+    expect(screen.getByTestId("work-list")).not.toBeUndefined();
+  });
+});
---
+++ tracker/src/WorkList.tsx
@@ -0,0 +1,5 @@
+function WorkList() {
+  return null;
+}
+
+export default WorkList;
