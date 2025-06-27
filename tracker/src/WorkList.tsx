fa583224a5baa94f72f6a87b47d3d037168df5fa
chaotictoejam <siroisjo@gmail.com>
2025-06-27 14:52:12 +0000

    Add handleClick functions

--- tracker/src/WorkList.tsx
+++ tracker/src/WorkList.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 function WorkList() {
+  const [workItem, setWorkItem] = useState([]);
+
+  function handleClick() {
+    setWorkItem([{}]);
+  }
+
   return (
     <div>
-      <button data-testid="btn-add"></button>
-      <div data-testid="work-list"></div>
+      <button data-testid="btn-add" onClick={handleClick}></button>
+      <div data-testid="work-list">
+        {workItem.map((value) => (
+          <div data-testid="work-item-1"></div>
+        ))}
+      </div>
     </div>
   );
 }
